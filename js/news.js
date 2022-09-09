d54a2f13fca3365f6a3c3abfd5a55f07dca3f311
Mamun-Ashrsf <ashrafdkia@gmail.com>
2022-09-09 07:33:24 +0600

    dynamic categories added

--- js/news.js
+++ js/news.js
@@ -10,18 +10,22 @@ const displayNewsCategories = newsCategories => {
     const categoriesUl = document.getElementById('news-categories');
     newsCategories.forEach(newsCategory => {
         const newsCategoryLi = document.createElement('li')
-        newsCategoryLi.innerText = `
+        /*newsCategoryLi.innerText = `
         ${newsCategory.category_name}
-        `;
+        `;*/
+        newsCategoryLi.innerHTML = `
+        <p onclick = "loadCategoriesDetails('${newsCategory.category_id}')">${newsCategory.category_name}</p>
+        `
         categoriesUl.appendChild(newsCategoryLi);
+
 
     });
 }
 
 loadNewsCategories();
 
-const loadCategoriesDetails = async () => {
-    const url = ` https://openapi.programming-hero.com/api/news/category/01`
+const loadCategoriesDetails = async (categoryId) => {
+    const url = `https://openapi.programming-hero.com/api/news/category/${categoryId}`
     const res = await fetch(url);
     const data = await res.json();
     displayCategoriesDetails(data.data);
@@ -45,7 +49,7 @@ const displayCategoriesDetails = (categoriesdetails) => {
             <div class ="d-flex justify-content-around">
                 <div class = "d-flex">
                     <img src = "${categoryDetails.author.img ? categoryDetails.author.img : "No image found"}" style = "height: 3rem; width:3rem; border-radius: 50%;">
-                    <h5>"${categoryDetails.author.name ? categoryDetails.author.name : "No name found"}"</h5>
+                    <h5>${categoryDetails.author.name ? categoryDetails.author.name : "No name found"}</h5>
                 </div>
                 <h5>Total view: ${categoryDetails.total_view ? categoryDetails.total_view : "No view found"}</h5>
                 <button class = "text-bg-primary px-5 py-2 rounded">Details</button>
